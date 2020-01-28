fbad9b0816a606e9bc0802adf0b7f0064d6df4c0
Fredrik Larsson <fredrik.larsson@medieinstitutet.se>
2020-01-28 10:59:33 +0100

    Added a template for the db connection

--- dbconn.php
+++ dbconn.php
@@ -1,4 +1,26 @@
 <?php
-// You must add your own dbconfig.php with all the info for your databases etc.
+// You must create your own dbconfig.php with all the info for your databases etc.
+
+// DBName must be declared like this: $dbname = "your_db_name"
+
+// $dbUser must look like this 
 require('dbconfig.php');
 
+
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "INSERT INTO MyGuests (firstname, lastname, email)
+    VALUES ('John', 'Doe', 'john@example.com')";
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    echo "New record created successfully";
+    }
+catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
+
+$conn = null;
